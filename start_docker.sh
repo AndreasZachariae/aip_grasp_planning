0c2451ee6d085bca62533f2eb5efca17c334a7d8
aip <mauricedroll@gmail.com>
2024-05-16 16:16:41 +0200

    Setup Environment, Git, Dockerfile

--- start_docker.sh
+++ start_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 uid=$(eval "id -u")
 gid=$(eval "id -g")
-
+       
+xhost + local:root
 docker run \
     --name aip_grasp_planning \
     --privileged \
@@ -9,6 +10,7 @@ docker run \
     -e DISPLAY=$DISPLAY \
     --env-file .env \
     -v $PWD/src:/home/robot/ros_ws/src:rw \
+    -v $PWD/pcl_recordings:/home/robot/ros_ws/pcl_recordings:rw \
     -v $PWD/.vscode:/home/robot/dependencies_ws/src/.vscode \
     -v $PWD/dependencies:/home/robot/dependencies_ws/src \
     -v /dev:/dev  \
@@ -16,3 +18,4 @@ docker run \
     --rm \
     --ipc host \
     iras/aip_grasp_planning:humble
+# --env-file .env \ set environment including ROS_DOMAIN_ID
