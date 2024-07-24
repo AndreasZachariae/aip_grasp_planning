890fc99e11cfe6076a618ae016023a6b6769a034
MauriceDroll <mauricedroll@gmail.com>
2024-07-24 16:02:14 +0200

    implemented plane segmentation

--- aip_grasp_planning/include/pcl_processing/point_cloud_processing_node.h
+++ aip_grasp_planning/include/pcl_processing/point_cloud_processing_node.h
@@ -6,9 +6,11 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/extract_indices.h>
 #include <rclcpp/rclcpp.hpp>
+#include <pcl/filters/voxel_grid.h>
 #include <geometry_msgs/msg/point.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
+#include <pcl/filters/project_inliers.h>
 #include <geometry_msgs/msg/transform.hpp>
 #include <aip_grasp_planning_interfaces/srv/grasp_object_surface_normal.hpp>
 #include <pcl/io/pcd_io.h>
@@ -20,8 +22,9 @@ public:
     void processPointCloud(const std::shared_ptr<aip_grasp_planning_interfaces::srv::GraspObjectSurfaceNormal::Request> request, std::shared_ptr<aip_grasp_planning_interfaces::srv::GraspObjectSurfaceNormal::Response> response);
 
 private:
+    void filterCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, float leaf_size);
     pcl::PointCloud<pcl::PointXYZ>::Ptr transformPointsToPointCloud(const std::vector<geometry_msgs::msg::Point>& maskedPoints);
-    pcl::ModelCoefficients::Ptr extractSurfacePlane(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud);
+    pcl::ModelCoefficients::Ptr extractSurfacePlane(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr& surfacePlane);
     pcl::PointCloud<pcl::PointXYZ>::Ptr extractInlierPoints(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, const pcl::PointIndices::Ptr inliers);
 
     rclcpp::Service<aip_grasp_planning_interfaces::srv::GraspObjectSurfaceNormal>::SharedPtr service;
--- aip_grasp_planning/src/point_cloud_processing_node.cpp
+++ aip_grasp_planning/src/point_cloud_processing_node.cpp
@@ -11,32 +11,57 @@
     {
         // Process the point cloud and generate the pose
         geometry_msgs::msg::Pose pose;
-        pose.position.x = 0.0;
-        pose.position.y = 0.0;
-        pose.position.z = 0.0;
-        pose.orientation.x = 0.0;
-        pose.orientation.y = 0.0;
-        pose.orientation.z = 0.0;
-        pose.orientation.w = 1.0;
+        
 
         // Extract the point cloud from the request
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
         cloud = this->transformPointsToPointCloud(request->masked_points);
-        
+        filterCloud(cloud, 0.01);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr surfacePlane(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::ModelCoefficients::Ptr planeCoefficients = extractSurfacePlane(cloud, surfacePlane);
         // Save the point cloud as a PCD file
         //
-        pcl::io::savePCDFileASCII("./src/aip_grasp_planning/cloud.pcd", *cloud);
+        // pcl::io::savePCDFileASCII("./src/aip_grasp_planning/cloud.pcd", *cloud);
         // pcl::ModelCoefficients::Ptr coefficients = this->extractSurfacePlane(cloud);
 
-        RCLCPP_INFO(this->get_logger(), "Point cloud saved as PCD file");
+        // RCLCPP_INFO(this->get_logger(), "Point cloud saved as PCD file");
+        pcl::PointXYZ averagePosition(0.0, 0.0, 0.0);
+        for (const auto& point : surfacePlane->points)
+        {
+            averagePosition.x += point.x;
+            averagePosition.y += point.y;
+            averagePosition.z += point.z;
+        }
+        int numPoints = surfacePlane->size();
+        averagePosition.x /= numPoints;
+        averagePosition.y /= numPoints;
+        averagePosition.z /= numPoints;
 
-
+        // Set the pose
+        pose.position.x = averagePosition.x;
+        pose.position.y = averagePosition.y;
+        pose.position.z = averagePosition.z;
+        pose.orientation.x = 0.0;
+        pose.orientation.y = 0.0;
+        pose.orientation.z = 0.0;
+        pose.orientation.w = 1.0;
+        // pose.position.x = planeCoefficients->values[0];
+        // pose.position.y = planeCoefficients->values[1];
+        // pose.position.z = planeCoefficients->values[2];
         // Set the response
         response->surface_normal_to_grasp = pose;
     }
 
     // rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr result_publisher_;
 
+
+void PointCloudProcessingNode::filterCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, float leaf_size)
+{
+        pcl::VoxelGrid<pcl::PointXYZ> voxelFilter;
+        voxelFilter.setInputCloud(cloud);
+        voxelFilter.setLeafSize(leaf_size, leaf_size, leaf_size);
+        voxelFilter.filter(*cloud);
+}
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudProcessingNode::transformPointsToPointCloud(const std::vector<geometry_msgs::msg::Point>& maskedPoints)
 {
@@ -58,7 +83,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudProcessingNode::transformPointsToP
     return cloud;
 }
 
-pcl::ModelCoefficients::Ptr PointCloudProcessingNode::extractSurfacePlane(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
+pcl::ModelCoefficients::Ptr PointCloudProcessingNode::extractSurfacePlane(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr& surfacePlane)
 {
     pcl::SACSegmentation<pcl::PointXYZ> seg;
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
@@ -73,6 +98,12 @@ pcl::ModelCoefficients::Ptr PointCloudProcessingNode::extractSurfacePlane(const 
     seg.setInputCloud(cloud);
     seg.segment(*inliers, *coefficients);
 
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+
+    extract.setInputCloud(cloud);
+    extract.setIndices(inliers);
+    extract.setNegative(false);
+    extract.filter(*surfacePlane);
     return coefficients;
 }
 
